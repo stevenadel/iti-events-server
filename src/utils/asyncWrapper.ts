939d6d509fffcf01605b9ac995b42e9ac878d99c
Steven Adel <stevenadel.dev@gmail.com>
2024-06-15 22:33:10 +0300

    Add async wrapper for more concise error handling

---
+++ src/utils/asyncWrapper.ts
@@ -0,0 +1,10 @@
+async function asyncWrapper<T>(promise: Promise<T>): Promise<[undefined, T] | Error> {
+    try {
+        const data = await promise;
+        return [undefined, data];
+    } catch (error) {
+        return error as Error;
+    }
+}
+
+export default asyncWrapper;
