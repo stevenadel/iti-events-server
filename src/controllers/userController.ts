1071f2419742e0f05d874a8b929af7f165432eaf
Steven Adel <stevenadel.dev@gmail.com>
2024-06-17 01:46:06 +0300

    WIP:  example register endpoint

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,9 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { UniqueConstraintError, ValidationError } from "sequelize";
 import User from "../models/User";
+import asyncWrapper from "../utils/asyncWrapper";
+import AppError from "../errors/AppError";
+import DataValidationError from "../errors/DataValidationError";
 
 export async function login(req: Request, res: Response) {
     res.json({
@@ -7,24 +11,32 @@ export async function login(req: Request, res: Response) {
     });
 }
 
-export async function register(req: Request, res: Response) {
-    try {
-        // dummy data placeholder
-        const userData = {
-            firstName: "John",
-            lastName: "Doe",
-            email: "johndoe@example.com",
-            password: "password123",
-            isActive: true,
-            role: "student",
-        };
+export async function register(req: Request, res: Response, next: NextFunction) {
+    // dummy data placeholder
+    const userData = {
+        firstName: "John",
+        lastName: "Doe",
+        email: "johndoe@example.com",
+        password: "password",
+        isActive: true,
+        role: "student",
+    };
 
-        const newUser = await User.create(userData);
+    const [error, newUser] = await asyncWrapper(User.create(userData));
 
-        res.status(201).json(newUser);
-    } catch (error) {
-        res.status(500).json(error);
+    if (error) {
+        if (error instanceof ValidationError) {
+            return next(new DataValidationError(error));
+        }
+
+        if (error instanceof UniqueConstraintError) {
+            return next(new AppError("Email already exists. Please use a different email", 409));
+        }
+
+        return next(new AppError("Database error. Please try again later."));
     }
+
+    res.status(201).json(newUser);
 }
 
 export async function getUsers(req: Request, res: Response) {
