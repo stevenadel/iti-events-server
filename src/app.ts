2504a3e9cc1de30fef7e0a9bc9cea14cb5260365
Steven Adel <stevenadel.dev@gmail.com>
2024-06-17 01:41:56 +0300

    Add error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDB from "./models";
 import routes from "./routes";
+import errorHandler from "./middlewares/errorHandler";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,6 +9,8 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errorHandler);
 
 app.listen(port, async () => {
     await connectDB();
