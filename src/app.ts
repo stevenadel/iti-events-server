47172dfb7ddabbb7643bd3eaca10ed0be7a8a962
Steven Adel <stevenadel.dev@gmail.com>
2024-06-19 17:57:03 +0300

    Add support for Mongoose and remove Sequelize from project

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,37 @@
 import express from "express";
-import connectDB from "./models";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import helmet from "helmet";
 import routes from "./routes";
 import errorHandler from "./middlewares/errorHandler";
 
+dotenv.config();
+
 const app = express();
-const port = process.env.PORT || 3000;
 
+app.use(helmet());
 app.use(express.json());
-
 app.use(routes);
-
 app.use(errorHandler);
 
-app.listen(port, async () => {
-    await connectDB();
-    console.log(`Server is running on port ${port}`);
+// For debugging, remove in production
+process.on("uncaughtException", (err) => {
+    console.log("Uncaught exception occurred:\n", err);
 });
+
+const {
+    PORT, DB_USERNAME, DB_PASSWORD, CLUSTER_URL, DB_NAME,
+} = process.env;
+const uri = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${CLUSTER_URL}/${DB_NAME}`;
+
+mongoose
+    .connect(uri)
+    .then(() => {
+        console.log("Connected to MongoDB Atlas");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port http://localhost:${PORT}/`);
+        });
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB Atlas:", err);
+    });
