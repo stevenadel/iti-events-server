5d7f7c5fdca5731a27d788796dbf7b0960038759
Salma158 <salmasherif856@gmail.com>
2024-06-20 20:52:24 +0300

    add buses points routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import userRouter from "./users";
 // import driverRouter from "./drivers";
 // import eventRouter from "./events";
 import busLineRouter from "./busLines";
-// import busPointRouter from "./busPoints";
+import busPointRouter from "./busPoints";
 
 const router = express.Router();
 
@@ -11,6 +11,6 @@ router.use("/users", userRouter);
 // router.use("/drivers", driverRouter);
 // router.use("/events", eventRouter);
 router.use("/buses/lines", busLineRouter);
-// router.use("/buses/points", busPointRouter);
+router.use("/buses/points", busPointRouter);
 
 export default router;
