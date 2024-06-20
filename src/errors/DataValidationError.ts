8f6eecd366a6b5dc4a718a3d9287e43ead9167ba
Steven Adel <stevenadel.dev@gmail.com>
2024-06-20 17:55:21 +0300

    Modify data validation error to return an object containing all errors

--- src/errors/DataValidationError.ts
+++ src/errors/DataValidationError.ts
@@ -1,14 +1,17 @@
-import { ValidationError } from "sequelize";
+import { Error } from "mongoose";
 import AppError from "./AppError";
 
-interface ErrorItem {
-    message: string;
-}
-
 class DataValidationError extends AppError {
-    constructor(error: ValidationError, status = 422) {
-        const messages = error.errors.map((e: ErrorItem) => e.message).join(" - ");
-        super(messages, status);
+    errors: { [field: string]: string };
+
+    constructor(error: Error.ValidationError, status = 422) {
+        super("Validation Error", status);
+
+        this.errors = {};
+
+        Object.keys(error.errors).forEach((field) => {
+            this.errors[field] = error.errors[field].message;
+        });
     }
 }
 
