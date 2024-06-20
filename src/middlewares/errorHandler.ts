d20ec0fcc2c79bdd6c11617d62a50a835f0361b4
Steven Adel <stevenadel.dev@gmail.com>
2024-06-20 17:55:56 +0300

    Modify error handler to support data validation error

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,20 +1,27 @@
 import { Request, Response, NextFunction } from "express";
 import AppError from "../errors/AppError";
+import DataValidationError from "../errors/DataValidationError";
 
 const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     let status = 500;
     let message = "Internal Server Error";
+    let errors: any | undefined;
 
     if (err instanceof AppError) {
-        status = (err as AppError).status;
+        status = err.status;
         message = err.message;
     }
 
-    // for debugging, remove in production
+    if (err instanceof DataValidationError) {
+        errors = err.errors;
+    }
+
+    // For debugging, remove in production
     console.error(err.stack);
 
     res.status(status).json({
         message,
+        errors
     });
 };
 
