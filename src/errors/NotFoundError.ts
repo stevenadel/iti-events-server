9efb746489ea7581cffa32dc2e3a075635ae28a1
Yousef Elsayed <yousef4511@outlook.com>
2024-06-21 18:36:57 +0300

    Document eventCategory api

--- src/errors/NotFoundError.ts
+++ src/errors/NotFoundError.ts
@@ -7,3 +7,25 @@ class NotFoundError extends AppError {
 }
 
 export default NotFoundError;
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     NotFoundError:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *           example: 'Not Found Error'
+ *         errors:
+ *           type: object
+ *           additionalProperties:
+ *             type: string
+ *             example: 'Field is required'
+ *       example:
+ *         message: 'Not Found Error'
+ *         errors:
+ *           fieldName: 'Field is required'
+ *           anotherField: 'Must be a valid email'
+ */
--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -12,7 +12,7 @@ const options = {
             },
         ],
     },
-    apis: ["./src/routes/*.ts", "./src/models/*.ts"],
+    apis: ["./src/routes/*.ts", "./src/models/*.ts", "./src/errors/*.ts"],
 };
 
 export default options;
