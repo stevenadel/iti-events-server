19d61e96d724d04208178ae41cc57ec767ddc7f4
Yousef Elsayed <yousef4511@outlook.com>
2024-06-22 16:28:27 +0300

    Add /users/me to get logged in user info

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,40 @@
 import { Router } from "express";
 import authenticateUser from "../middlewares/authenticateUser";
 import isAdmin from "../middlewares/isAdmin";
-import { getAllUsers } from "../controllers/userController";
+import { getAllUsers, getMe } from "../controllers/userController";
 
 const router = Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: User management
+ */
+
 router.get("/", authenticateUser, isAdmin, getAllUsers);
+
+/**
+ * @swagger
+ * /users/me:
+ *   get:
+ *     summary: Get current logged in user info
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: Logged in user info
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 user:
+ *                   $ref: '#/components/schemas/User'
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
+router.get("/me", authenticateUser, getMe);
 
 export default router;
