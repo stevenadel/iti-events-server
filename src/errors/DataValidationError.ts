0c42e48f88632abd0d2b42c1b7988d9c80159d67
Steven Adel <stevenadel.dev@gmail.com>
2024-06-23 01:47:05 +0300

    Revert changes to data validation error

--- src/errors/DataValidationError.ts
+++ src/errors/DataValidationError.ts
@@ -1,16 +1,17 @@
 import { Error } from "mongoose";
 import AppError from "./AppError";
-import Errors from "../types/Errors";
 
 class DataValidationError extends AppError {
+    errors: { [field: string]: string };
+
     constructor(error: Error.ValidationError, status = 422) {
-        const errors: Errors = {};
+        super("Validation Error", status);
+
+        this.errors = {};
 
         Object.keys(error.errors).forEach((field) => {
             this.errors[field] = error.errors[field].message;
         });
-
-        super("Validation Error", status, errors);
     }
 }
 
