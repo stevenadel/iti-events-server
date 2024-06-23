9345efb7921a1080da3adf9a49f08f93595e9aca
Yousef Elsayed <yousef4511@outlook.com>
2024-06-24 00:08:10 +0300

    Add support to upload images to cloud [cloudinary]

---
+++ src/middlewares/uploadImageToCloudinary.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from "express";
+import cloudinary from "../configs/cloudinary.config";
+
+const uploadSingleImageToCloud = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        if (!req.file) {
+            next();
+            return;
+        }
+        const stream = cloudinary.uploader.upload_stream({ folder: "uploads" }, (err, result) => {
+            if (err) {
+                next(err);
+                return;
+            }
+
+            if (!result) {
+                next(new Error("Cloudinary upload failed with result undefined"));
+                return;
+            }
+
+            req.body.imageUrl = result.secure_url;
+            console.log(`Uploaded image with url [${req.body.imageUrl}]`);
+            req.body.cloudinaryPublicId = result.public_id;
+            next();
+        });
+        stream.end(req.file?.buffer);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export default uploadSingleImageToCloud;
