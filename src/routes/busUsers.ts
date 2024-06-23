6f60da6718bed1c01cd5aa125f255c0f4b653a0d
Salma158 <salmasherif856@gmail.com>
2024-06-23 23:29:28 +0300

    added get all users subscribed in bus

--- src/routes/busUsers.ts
+++ src/routes/busUsers.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { subscribe, unsubscribe } from "../controllers/busUsersController";
+import { subscribe, unsubscribe, getAllBusUsers } from "../controllers/busUsersController";
 
 const router = express.Router();
 
 router.post("/", subscribe);
 router.delete("/", unsubscribe);
+router.get("/:busLineId", getAllBusUsers);
 
 export default router;
