35f0afabd1c0a699cb26053b51d27982b8077976
Salma158 <salmasherif856@gmail.com>
2024-06-23 23:20:03 +0300

    added unsubscibtion logic

--- src/routes/busUsers.ts
+++ src/routes/busUsers.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { subscribe } from "../controllers/busUsersController";
+import { subscribe, unsubscribe } from "../controllers/busUsersController";
 
 const router = express.Router();
 
 router.post("/", subscribe);
+router.delete("/", unsubscribe);
 
 export default router;
