d52a4cbf9f21f5540460630b9b490d4d3a394b73
Yousef Elsayed <yousef4511@outlook.com>
2024-06-25 02:08:15 +0300

    Add endpoint to approve attendees

--- src/controllers/attendeeController.ts
+++ src/controllers/attendeeController.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { isValidObjectId } from "mongoose";
-import { getAllAttendees, getAttendeeById } from "../services/attendeeService";
+import { getAllAttendees, getAttendeeById, updateAttendeeApprovalStatus } from "../services/attendeeService";
 import ValidationError from "../errors/ValidationError";
 import NotFoundError from "../errors/NotFoundError";
 
@@ -27,6 +27,27 @@ export const attendeeById = async (req: Request, res: Response, next: NextFuncti
 
         if (!attendee) {
             next(new NotFoundError("Attendee Not Found"));
+            return;
+        }
+
+        res.json({ attendee });
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const approveAttendee = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { attendeeId } = req.params;
+        if (!isValidObjectId(attendeeId)) {
+            next(new ValidationError("Invalid attendee id format"));
+            return;
+        }
+
+        const attendee = await updateAttendeeApprovalStatus(attendeeId, true);
+
+        if (!attendee) {
+            next(new NotFoundError("Attendee does not exist"));
             return;
         }
 
