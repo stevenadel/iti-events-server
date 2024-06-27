745b9cfb1ce60374a7d778f46be70587073f7a9f
Yousef Ghonim <37050129+yousef8@users.noreply.github.com>
2024-06-27 09:35:10 +0000

    Remove image feature from create bus line

--- src/routes/busLines.ts
+++ src/routes/busLines.ts
@@ -2,12 +2,11 @@ import express from "express";
 import { createBusLine, getBusLines, getBusLineById, updateBusLine, deleteBusLine} from "../controllers/busLineController";
 import authenticateUser from "../middlewares/authenticateUser";
 import isAdmin from "../middlewares/isAdmin";
-import parseFormWithSingleImage from "../middlewares/parseFormWithSingleImage";
 
 const router = express.Router();
 
 router.get("/",authenticateUser, getBusLines);
-router.post("/", authenticateUser, isAdmin, parseFormWithSingleImage(),  createBusLine);
+router.post("/", authenticateUser, isAdmin, createBusLine);
 
 router.get("/:id",authenticateUser,  getBusLineById);
 router.put("/:id", authenticateUser, isAdmin,  updateBusLine);
